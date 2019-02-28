3da02968726d52088c0777321fbde08cbbffc188
Carlos <camartto@users.noreply.github.com>
2019-02-28 17:41:07 +0100

    Initial commit

---
+++ app/src/main/java/android/example/com/implicitintents/ImplicitIntentsReceiver.java
@@ -0,0 +1,13 @@
+package android.example.com.implicitintents;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ImplicitIntentsReceiver extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_implicit_intents_receiver);
+    }
+}
